44b87d499ac4305feed1c3e17fc7aa6e840a239d
Cristian Andres MQ <64241547+cristian-andres-mq@users.noreply.github.com>
2023-05-04 22:28:03 +0000

    Add bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
+import "bootswatch/dist/minty/bootstrap.min.css";
 import DisplayData from "@/components/DisplayData.jsx";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import Head from "next/head";
-// #EE2B7B Color Magenta comfama 
+// #EE2B7B Color Magenta comfama
 // #D9D9D9 Color Gris comfama
-
 
 function App() {
   return (
@@ -12,10 +11,6 @@ function App() {
       <Head>
         <title>Generador de Muestras</title>
         <meta name="description" content="Generador de Muestras" />
-        <link
-          rel="stylesheet"
-          href="https://bootswatch.com/5/minty/bootstrap.min.css"
-        />
         <link rel="icon" href="/letter-c.png" />
       </Head>
 
